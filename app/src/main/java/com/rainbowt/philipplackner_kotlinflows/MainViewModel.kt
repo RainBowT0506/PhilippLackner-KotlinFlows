26bc81c90f366b1233c9cf00803fc5ad6044590d
RainBowT0506 <rainbowt0506@gmail.com>
2024-08-14 16:13:23 +0800

    feat: Add multiple flow operators in ViewModel
    - Added methods for collecting flows with various operators: `count`, `reduce`, `fold`, `flatMapConcat`, and more
    - Implemented `collectFlowWithCount` to filter, map, and count emissions
    - Added `collectFlowWithReduce` for summing values using `reduce`
    - Implemented `collectFlowWithFold` to sum values with an initial value using `fold`
    - Added `collectFlowWithFlatMapConcat` to demonstrate `flatMapConcat`
    - Implemented `collectFlowWithFlat` for handling multiple emissions with `buffer`, `conflate`, and `collectLatest`
    - Demonstrated usage of different flow operators and their effects on emissions

--- app/src/main/java/com/rainbowt/philipplackner_kotlinflows/MainViewModel.kt
+++ app/src/main/java/com/rainbowt/philipplackner_kotlinflows/MainViewModel.kt
@@ -3,7 +3,19 @@ package com.rainbowt.philipplackner_kotlinflows
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.fold
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.reduce
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
@@ -21,14 +33,97 @@ class MainViewModel : ViewModel() {
     }
 
     init {
-        collectFlow()
+        collectFlowWithCount()
+        collectFlowWithReduce()
+        collectFlowWithFold()
+        collectFlowWithFlatMapConcat()
+        collectFlowWithFlat()
     }
 
-    private fun collectFlow() {
+    private fun collectFlowWithCount() {
         viewModelScope.launch {
-            countDownFlow.collect { time ->
-                println("The current time is $time")
+            val count = countDownFlow
+                .filter { time ->
+                    time % 2 == 0
+                }
+                .map { time ->
+                    time * time
+                }
+                .onEach { time ->
+                    println(time)
+                }
+                .count {
+                    it % 2 == 0
+                }
+
+            println("The count is $count")
+        }
+    }
+
+    private fun collectFlowWithReduce() {
+        viewModelScope.launch {
+            val reduceResult = countDownFlow
+                .reduce { accumulator, value ->
+                    accumulator + value
+                }
+
+
+            println("Reduce - The sum of squares is $reduceResult")
+        }
+    }
+
+    private fun collectFlowWithFold() {
+        viewModelScope.launch {
+            val foldResult = countDownFlow
+                .fold(100) { accumulator, value ->
+                    accumulator + value
+                }
+
+            println("Fold - The sum of squares is $foldResult")
+        }
+    }
+
+    private fun collectFlowWithFlatMapConcat() {
+        val flow1 = flow {
+            emit(1)
+            delay(1000)
+            emit(2)
+        }
+
+        viewModelScope.launch {
+            flow1.flatMapConcat { value ->
+                flow {
+                    emit(value + 1)
+                    delay(1000)
+                    emit(value + 2)
+                }
+            }.collect { value ->
+                println("FlatMapConcat - The value is $value")
             }
+        }
+    }
+
+    private fun collectFlowWithFlat() {
+        val flow = flow {
+            delay(250L)
+            emit("Appetizer")
+            delay(1000L)
+            emit("Main dish")
+            delay(100L)
+            emit("Dessert")
+        }
+
+        viewModelScope.launch {
+            flow.onEach {
+                println("FLOW: $it is delivered")
+            }
+                .buffer()
+                .conflate()
+                .collectLatest {
+                    println("FLOW: Now eating $it")
+                    delay(1500L)
+                    println("FLOW: Finished eating $it")
+                }
         }
     }
 }
