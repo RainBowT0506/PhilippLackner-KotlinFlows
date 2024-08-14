c9e5a0a3b805b3e9636473bd93a5b1ddc3b62ce9
RainBowT0506 <rainbowt0506@gmail.com>
2024-08-14 14:41:09 +0800

    chore: Add Kotlin Flow countdown timer in MainViewModel
    - Implemented a countdown timer using Kotlin Flow
    - Added `countDownFlow` emitting values from 10 to 0 with a 1-second delay
    - Created `collectFlow` to collect and print emitted values
    - Initialized flow collection in ViewModel's init block

--- app/src/main/java/com/rainbowt/philipplackner_kotlinflows/MainActivity.kt
+++ app/src/main/java/com/rainbowt/philipplackner_kotlinflows/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.rainbowt.philipplackner_kotlinflows.ui.theme.PhilippLacknerKotlinFlowsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PhilippLacknerKotlinFlowsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val viewModel = viewModel<MainViewModel>()
+                val time = viewModel.countDownFlow.collectAsState(initial = 10)
+
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Text(
+                        text = time.value.toString(),
+                        fontSize = 30.sp,
+                        modifier = Modifier.align(Alignment.Center)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhilippLacknerKotlinFlowsTheme {
-        Greeting("Android")
     }
 }
